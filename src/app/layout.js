0bc0193fd1c963f9ab0ee45c5d4bb087206c7bd5
ogabek <ogbkiy@gmail.com>
2025-06-07 21:20:13 +0500

    pixel code added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Montserrat } from "next/font/google";
+import Script from "next/script"; 
 
 const montserrat = Montserrat({
   variable: "--font-montserrat",
@@ -9,34 +10,32 @@ export const metadata = {
   title: "Недвижимость в Турции | FARZZ GROUP - Купить квартиру в Стамбуле",
   description:
     "FARZZ GROUP — Надёжный партнёр для покупки недвижимости в Турции. Помощь в оформлении ВНЖ и гражданства. Рассрочка, готовые и инвестиционные объекты в Стамбуле и других городах. Говорим на вашем языке.",
-    keywords: [
-      // Ruscha
-      "недвижимость в Турции",
-      "купить квартиру в Стамбуле",
-      "жилье в Турции",
-      "инвестиции в Турции",
-      "ВНЖ в Турции",
-      "гражданство Турции",
-      "недвижимость Стамбул",
-      "рассрочка на квартиру",
-      "русскоязычный риелтор Турция",
-      "купить жилье в Турции",
-      "FARRZ GROUP",
-      "farzz group",
-      "farz group",
-      "farzzgroup",
-      "Turkiyada ko‘chmas mulk",
-      "Turkiyadan uy olish",
-      "Istanbuldan kvartira olish",
-      "Turkiyada kvartira narxlari",
-      "Turkiyada yashash uchun uy",
-      "Turkiyada VNZ olish",
-      "Turkiyadan kvartira sotib olish",
-      "Turkiyada investitsiya qilish",
-      "Turkiyada uy olish shartlari",
-      "Turkiyada ko‘chmas mulk agentligi",
-    ]
-,    
+  keywords: [
+    "недвижимость в Турции",
+    "купить квартиру в Стамбуле",
+    "жилье в Турции",
+    "инвестиции в Турции",
+    "ВНЖ в Турции",
+    "гражданство Турции",
+    "недвижимость Стамбул",
+    "рассрочка на квартиру",
+    "русскоязычный риелтор Турция",
+    "купить жилье в Турции",
+    "FARRZ GROUP",
+    "farzz group",
+    "farz group",
+    "farzzgroup",
+    "Turkiyada ko‘chmas mulk",
+    "Turkiyadan uy olish",
+    "Istanbuldan kvartira olish",
+    "Turkiyada kvartira narxlari",
+    "Turkiyada yashash uchun uy",
+    "Turkiyada VNZ olish",
+    "Turkiyadan kvartira sotib olish",
+    "Turkiyada investitsiya qilish",
+    "Turkiyada uy olish shartlari",
+    "Turkiyada ko‘chmas mulk agentligi",
+  ],
   authors: [{ name: "FARZZ GROUP", url: "https://farzzgroup.com" }],
   creator: "FARZZ GROUP",
   publisher: "FARZZ GROUP",
@@ -48,7 +47,7 @@ export const metadata = {
     siteName: "FARZZ GROUP",
     images: [
       {
-        url: "/hero.jpeg", 
+        url: "/hero.jpeg",
         width: 1200,
         height: 630,
         alt: "FARZZ GROUP - Недвижимость в Турции",
@@ -57,7 +56,6 @@ export const metadata = {
     locale: "ru",
     type: "website",
   },
-
   icons: {
     icon: "/logo.png",
     shortcut: "/favicon.ico",
@@ -65,12 +63,43 @@ export const metadata = {
   },
 };
 
-
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${montserrat.variable} antialiased`}>{children}</body>
+      <head>
+        {/* ✅ Facebook Pixel script */}
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '722783247095632');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+      </head>
+      <body className={`${montserrat.variable} antialiased`}>
+        {/* 📸 noscript backup image */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=722783247095632&ev=PageView&noscript=1"
+            alt="fb pixel"
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
